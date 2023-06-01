f7c9d5ec8615961ba828097542403844190d7453
Eray Yilmaz <erayyillmaz1@gmail.com>
2023-06-01 16:52:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
 import { Component } from '@angular/core';
+import {
+  IMqttMessage,
+  IMqttServiceOptions,
+  MqttService,
+  IPublishOptions,
+} from 'ngx-mqtt';
+import { IClientSubscribeOptions } from 'mqtt-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  constructor(private _mqttService: MqttService) {
+    this.client = this._mqttService;
+  }
+  private curSubscription: Subscription | undefined;
+  connection = {
+    hostname: 'broker.emqx.io',
+    port: 8083,
+    path: '/mqtt',
+    clean: true, 
+    connectTimeout: 4000, 
+    reconnectPeriod: 4000, 
+    clientId: 'mqttx_597046f4',
+    username: 'emqx_test',
+    password: 'emqx_test',
+    protocol: 'ws',
+    }
+  subscription = {
+    topic: 'topic/mqttx',
+    qos: 0,
+  };
+  publish = {
+    topic: 'topic/browser',
+    qos: 0,
+    payload: '{ "msg": "Hello, I am browser." }',
+  };
+  receiveNews = '';
+  qosList = [
+    { label: 0, value: 0 },
+    { label: 1, value: 1 },
+    { label: 2, value: 2 },
+  ];
+  client: MqttService | undefined;
+  isConnection = false;
+  subscribeSuccess = false;
+
+  createConnection() {
+    try {
+      this.client?.connect(this.connection as IMqttServiceOptions)
+    } catch (error) {
+      console.log('mqtt.connect error', error);
+    }
+    this.client?.onConnect.subscribe(() => {
+      this.isConnection = true
+      console.log('Connection succeeded!');
+    });
+    this.client?.onError.subscribe((error: any) => {
+      this.isConnection = false
+      console.log('Connection failed', error);
+    });
+    this.client?.onMessage.subscribe((packet: any) => {
+      this.receiveNews = this.receiveNews.concat(packet.payload.toString())
+      console.log(`Received message ${packet.payload.toString()} from topic ${packet.topic}`)
+    })
+  }
+
+
+  doSubscribe() {
+    const { topic, qos } = this.subscription
+    this.curSubscription = this.client?.observe(topic, { qos } as IClientSubscribeOptions).subscribe((message: IMqttMessage) => {
+      this.subscribeSuccess = true
+      console.log('Subscribe to topics res', message.payload.toString())
+    })
+  }
+  doUnSubscribe() {
+    this.curSubscription?.unsubscribe()
+    this.subscribeSuccess = false
+  }
+  doPublish() {
+    const { topic, qos, payload } = this.publish
+    console.log(this.publish)
+    this.client?.unsafePublish(topic, payload, { qos } as IPublishOptions)
+  }
+  destroyConnection() {
+    try {
+      this.client?.disconnect(true)
+      this.isConnection = false
+      console.log('Successfully disconnected!')
+    } catch (error: any) {
+      console.log('Disconnect failed', error.toString())
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
 
+export const connection: IMqttServiceOptions = {
+  hostname: 'broker.emqx.io',
+  port: 8083,
+  path: '/mqtt',
+  clean: true, 
+  connectTimeout: 4000, 
+  reconnectPeriod: 4000,
+  clientId: 'mqttx_597046f4',
+  username: 'emqx_test',
+  password: 'emqx_test',
+  protocol: 'ws',
+  connectOnCreate: false,
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatSelectModule,
+    MqttModule.forRoot(connection)
   ],
   providers: [],
   bootstrap: [AppComponent]
